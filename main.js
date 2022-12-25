a628602441bafb2d9f9800162075e390f76f0616
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-12-25 13:36:14 +0545

    Road Generation

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ canvas.width=200;
 
 const ctx = canvas.getContext("2d");
 //x =100, y=100, w=30, h=50 
-const car = new Car(100, 100,30, 50);
+
+const road = new Road(canvas.width/2, canvas.width*0.9)
+const car = new Car(road.getLaneCenter(1), 100,30, 50);
+// const car = new Car(100, 100,30, 50);
 car.draw(ctx);
 
 
@@ -15,6 +18,13 @@ animate();
 function animate(){
     car.update();
     canvas.height = window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0, -car.y+ canvas.height*0.7);
+
+    road.draw(ctx);
     car.draw(ctx);
+
+    ctx.restore();
     requestAnimationFrame(animate);
 }
